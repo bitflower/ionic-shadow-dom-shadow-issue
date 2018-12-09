366561951b1fdd8cd6f6d0915a1ab8c9e7fc965b
bitflower <matthias.max@bitflower.net>
2018-12-09 20:27:40 +0100

    add split-pane and menu

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -8,22 +8,58 @@ import { Component } from '@stencil/core';
 })
 export class AppRoot {
 
+  // @Prop({
+  //   connect: 'ion-menu-controller'
+  // })
+  // private menuCtrl: HTMLIonMenuControllerElement;
+
+  async componentDidUnload() {
+    // await this.menuCtrl.open('start');
+  }
+
   render() {
     return (
-      <div>
-        <header>
-          <h1>Stencil App Starter</h1>
-        </header>
+      <ion-split-pane when={false}>
+        <ion-menu
+          side='start'
+          swipeGesture={true}
+          type='reveal'
+        >
+          <ion-header>
+            <ion-toolbar>
+              <ion-title>My menu</ion-title>
+            </ion-toolbar>
+          </ion-header>
 
-        <main>
-          <stencil-router>
-            <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url='/' component='app-home' exact={true} />
-              <stencil-route url='/profile/:name' component='app-profile' />
-            </stencil-route-switch>
-          </stencil-router>
-        </main>
-      </div>
+          <ion-content>
+            <p>This is the menu content.</p>
+          </ion-content>
+
+        </ion-menu >
+
+        <ion-content main scrollX={false} scrollY={false}>
+          <div>
+            <main>
+              <ion-header>
+                <ion-toolbar>
+                  <ion-buttons slot='start'>
+                    {/* <ion-menu-button /> */}
+                  </ion-buttons>
+                  <ion-title>
+                    Stencil App Starter
+                </ion-title>
+                </ion-toolbar>
+              </ion-header>
+              <stencil-router>
+                <stencil-route-switch scrollTopOffset={0}>
+                  <stencil-route url='/' component='app-home' exact={true} />
+                  <stencil-route url='/profile/:name' component='app-profile' />
+                </stencil-route-switch>
+              </stencil-router>
+            </main>
+          </div>
+        </ion-content >
+      </ion-split-pane >
     );
   }
 }
